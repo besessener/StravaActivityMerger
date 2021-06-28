5a5ff7026ed805813560a0764a9d9eeb41e4093b
Eggert Matthias (uidw8852) <Matthias.Eggert@continental-corporation.com>
2021-06-28 08:34:14 +0200

    follow http redirects

--- src/main/groovy/me/strava/activitymerger/authentication/AuthentificationHandler.groovy
+++ src/main/groovy/me/strava/activitymerger/authentication/AuthentificationHandler.groovy
@@ -3,23 +3,33 @@ package me.strava.activitymerger.authentication
 
 import groovyx.net.http.HTTPBuilder
 import me.strava.activitymerger.helper.AppConstants
+import me.strava.activitymerger.helper.Secrets
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.impl.client.LaxRedirectStrategy
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 class AuthentificationHandler {
 
+    @Autowired
+    Secrets secrets
+
     String getStravaAuthentificationUrl() {
-        "${AppConstants.STRAVA_BASE_URL}oauth/authorize?client_id=${AppConstants.getSecret('client_id')}&response_type=code&redirect_uri=${AppConstants.STRAVA_REDIRECT_URL}&approval_prompt=force&scope=activity:read_all"
+        "${AppConstants.STRAVA_BASE_URL}oauth/authorize?client_id=${secrets.getSecret('client_id')}&response_type=code&redirect_uri=${AppConstants.STRAVA_REDIRECT_URL}&approval_prompt=force&scope=activity:read_all"
     }
 
     String getAuthToken(String exchangeToken) {
         def accessToken = ''
         def http = new HTTPBuilder(AppConstants.STRAVA_BASE_URL)
+        def httpClient = new HttpClientBuilder()
+        httpClient.setRedirectStrategy(LaxRedirectStrategy.newInstance())
+        http.client = httpClient.build()
         http.post(
                 path: '/oauth/token',
                 body: [
-                        client_id    : AppConstants.getSecret('client_id'),
-                        client_secret: AppConstants.getSecret('client_secret'),
+                        client_id    : secrets.getSecret('client_id'),
+                        client_secret: secrets.getSecret('client_secret'),
                         grant_type   : "authorization_code",
                         code         : exchangeToken
                 ]) { resp, reader ->
--- src/main/groovy/me/strava/activitymerger/helper/AppConstants.groovy
+++ src/main/groovy/me/strava/activitymerger/helper/AppConstants.groovy
@@ -5,15 +5,4 @@ import groovy.json.JsonSlurper
 class AppConstants {
     static final String STRAVA_BASE_URL = 'http://www.strava.com/'
     static final String STRAVA_REDIRECT_URL = 'http://localhost/exchangeToken'
-
-    static def secrets
-
-    static def getSecret(def key) {
-        if (!secrets) {
-            def secretsFile = new File(getClass().getResource('/.secrets').toURI())
-            secrets = new JsonSlurper().parse(secretsFile)
-        }
-
-        secrets."$key"
-    }
 }
