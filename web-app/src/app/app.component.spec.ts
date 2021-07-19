b93a216dfc9f5924d3823fb81269e4ed9d2d774c
Eggert Matthias (uidw8852) <Matthias.Eggert@continental-corporation.com>
2021-07-19 11:20:45 +0200

    add one more app test

--- web-app/src/app/app.component.spec.ts
+++ web-app/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ActivitiesRetrieverService} from "./services/activities-retriever/activities-retriever.service";
+import {Activity} from "./components/activity-table/activity-table.component";
 
 describe('AppComponent', () => {
+  let activitiesRetrieverService: ActivitiesRetrieverService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -14,6 +18,8 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    activitiesRetrieverService = TestBed.inject(ActivitiesRetrieverService);
   });
 
   it('should create the app', () => {
@@ -27,4 +33,20 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Activity Merger');
   });
+
+  it('activities subsription', fakeAsync(() => {
+    localStorage.clear();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.tokenAvailable = false;
+    app.codeAvailable = false;
+
+    let data: Activity = {id: 1, type: 'ride', name: 'afternoon ride', date: '1-July-2021', elapsedTime: '10 seconds'};
+    activitiesRetrieverService.activities.next(data);
+
+    tick(1);
+
+    expect(app.tokenAvailable).toBeTrue();
+    expect(app.codeAvailable).toBeFalse();
+  }))
 });
